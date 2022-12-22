30fe8b9dbbae97b61139d3731d3db9836da4fe23
Michael Caxias <michaelcaxias.dev@gmail.com>
2022-12-22 16:49:11 -0300

    :wrench: chore: return Teste.test.tsx to old state

--- src/__tests__/Teste.test.tsx
+++ src/__tests__/Teste.test.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import Teste from '../components/Teste';
-// import App from '../App';
+import App from '../App';
 
 describe('loads and displays greeting', () => {
   it('Teste', () => {
@@ -16,14 +16,14 @@ describe('loads and displays greeting', () => {
     expect(testElement).toHaveTextContent('Teste');
   });
 
-  // it('App', () => {
-  //   // ARRANGE
-  //   render(<App />);
+  it('App', () => {
+    // ARRANGE
+    render(<App />);
 
-  //   // ACT
-  //   const testElement = screen.getByTestId('app');
+    // ACT
+    const testElement = screen.getByTestId('app');
 
-  //   // ASSERT
-  //   expect(testElement).toHaveTextContent('Hello, world!');
-  // });
+    // ASSERT
+    expect(testElement).toHaveTextContent('Hello, world!');
+  });
 });
