116f213a8d7a7c81cf2ecfd454f0926e4834dadb
Michael Caxias <michael.caxias@mercadolivre.com>
2022-12-24 18:13:58 -0300

    :sparkles: feat: login page first layout

--- src/components/pages/Login/index.tsx
+++ src/components/pages/Login/index.tsx
@@ -2,6 +2,7 @@ import React, { useRef } from 'react';
 import Input from '../../Input';
 import { Form } from '@unform/web';
 import { FormHandles } from '@unform/core';
+import { Link } from 'react-router-dom';
 
 export default function Login() {
   const formRef = useRef<FormHandles>(null);
@@ -19,14 +20,36 @@ export default function Login() {
   };
 
   return (
-    <div>
-      <h1>Login</h1>
-      <Form onSubmit={handleSubmit} ref={formRef}>
-        <Input type="text" name="login" data-testid="login" />
-        <Input type="password" name="password" data-testid="password" />
-        <button type="submit" data-testid="login-btn">
-          Logar
+    <div className="login-page">
+      <Form onSubmit={handleSubmit} ref={formRef} className="form-login">
+        <div className="login-input-group">
+          <Input
+            type="text"
+            name="login"
+            data-testid="login"
+            className="input-login"
+            placeholder="Login"
+          />
+          <Input
+            type="password"
+            name="password"
+            data-testid="password"
+            className="input-login"
+            placeholder="Senha"
+          />
+        </div>
+        <Link to="/recover-password">Esqueci minha senha</Link>
+        <button type="submit" data-testid="login-btn" className="submit-login">
+          Entrar
         </button>
+        <span className="register-link">
+          Não tem uma conta? <Link to="/register">Registre-se</Link>
+        </span>
+        <hr className="separator" />
+        <div>
+          <span className='try-login-with'>Ou entre com</span>
+          <button type="button">Google</button>
+        </div>
       </Form>
     </div>
   );
